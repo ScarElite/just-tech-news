41bb86e91cfdb92b371bbec1e0ce2b89959eafb6
Mitchell Armstrong <mra0211@gmail.com>
2022-10-23 20:53:20 -0400

    Conditionally rendered the login links based on whether a user is logged in or out

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,7 +35,10 @@ router.get("/", (req, res) => {
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
       // Pass a single post object into the homepage template
-      res.render("homepage", { posts });
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -94,7 +97,10 @@ router.get("/post/:id", (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render("single-post", { post });
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
